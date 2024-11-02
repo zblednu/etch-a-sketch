31f844a1c9a8ece850083477d5d6ea907e769d5b
zblednu <aguy>
2024-11-02 13:49:24 +0100

    prevent page refresh on scrolling down(hopefully)

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
 "use strict"
+
 const gameField = document.querySelector(".game-field");
 
 /* BOARD INIT */
 const lowResDim = 20;
 const highResDim = 35;
 document.querySelector(".low-res-btn").addEventListener("click", (event) => {
-        event.stopPropagation();
-        initBoard(lowResDim);
+    event.stopPropagation();
+    initBoard(lowResDim);
 });
 
 document.querySelector(".high-res-btn").addEventListener("click", (event) => {
-        event.stopPropagation();
-        initBoard(highResDim);
+    event.stopPropagation();
+    initBoard(highResDim);
 });
 
 function initBoard(dim) {
-        gameField.innerHTML = "";
-        gameField.style.gridTemplateColumns = `repeat(${dim}, 1fr)`;
-        gameField.style.gridTemplateRows = `repeat(${dim}, 1fr)`;
+    gameField.innerHTML = "";
+    gameField.style.gridTemplateColumns = `repeat(${dim}, 1fr)`;
+    gameField.style.gridTemplateRows = `repeat(${dim}, 1fr)`;
 
-        for (let i = 0; i < dim ** 2; ++i) {
-            const tile = document.createElement("div");
-            tile.className = "tile";
-            gameField.appendChild(tile);
-        }
+    for (let i = 0; i < dim ** 2; ++i) {
+        const tile = document.createElement("div");
+        tile.className = "tile";
+        gameField.appendChild(tile);
+    }
 }
 
 initBoard(lowResDim);
 
 /* DRAWING LOGIC */
 document.addEventListener("touchmove", (event) => {
-        event.preventDefault();  // Prevent scrolling
-        const touch = event.touches[0];
-        const targetTile = document.elementFromPoint(touch.clientX, touch.clientY);
-        if (targetTile && targetTile.classList.contains("tile")) {
-                targetTile.classList.add("visited");
-        }
-});
+    event.preventDefault();  // Prevent scrolling
+    const touch = event.touches[0];
+    const targetTile = document.elementFromPoint(touch.clientX, touch.clientY);
+    if (targetTile && targetTile.classList.contains("tile")) {
+            targetTile.classList.add("visited");
+    }
+}, {passive: false});
 
 document.querySelector(".reset").addEventListener("click", (e) => resetBoard(e));
 
